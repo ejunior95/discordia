6b9777a26bcfe2838af18b8560549da69caec863
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-10 01:13:54 -0300

    feat: Criando estrutura da controller com método para testar a integração com Chat GPT

--- src/chat-gpt/chat-gpt.controller.ts
+++ src/chat-gpt/chat-gpt.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpStatus, Post, Req, Res } from '@nestjs/common';
+import { ChatGptService } from './chat-gpt.service';
+import { Request, Response } from 'express';
 
 @Controller('chat-gpt')
-export class ChatGptController {}
+export class ChatGptController {
+    constructor(
+        private readonly chatGptService: ChatGptService,
+    ) {}
+
+    @Post('/test-message')
+    async testMessage(@Req() req: Request, @Res() res: Response) {
+        try {
+            const result = await this.chatGptService.execute();
+            return res.status(HttpStatus.OK).json(result);
+        } catch (error) {
+            return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(error);
+        }
+    }
+}
