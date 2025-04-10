e6589cc9bc3ee63033c68ad29b768ce8c110fd26
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-10 01:14:18 -0300

    feat: Criando estrutura da controller com método para testar a integração com Deepseek

--- src/deepseek/deepseek.controller.ts
+++ src/deepseek/deepseek.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpStatus, Post, Req, Res } from '@nestjs/common';
+import { DeepseekService } from './deepseek.service';
+import { Request, Response } from 'express';
 
 @Controller('deepseek')
-export class DeepseekController {}
+export class DeepseekController {
+    constructor(
+        private readonly deepSeekService: DeepseekService,
+    ) {}
+
+    @Post('/test-message')
+    async testMessage(@Req() req: Request, @Res() res: Response) {
+        try {
+            const result = await this.deepSeekService.execute();
+            return res.status(HttpStatus.OK).json(result);
+        } catch (error) {
+            return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(error);
+        }
+    }
+}
