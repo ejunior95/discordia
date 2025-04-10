b4fae3f2484fbd948ed28e95000ed2f47acbdeda
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-10 01:14:29 -0300

    feat: Criando estrutura da service com método para testar a integração com Deepseek

--- src/deepseek/deepseek.service.ts
+++ src/deepseek/deepseek.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
-
+import OpenAI from "openai";
 @Injectable()
-export class DeepseekService {}
+export class DeepseekService {
+    aiInstance = new OpenAI({
+        baseURL: process.env.DEEPSEEK_API_BASE_URL,
+        apiKey: process.env.DEEPSEEK_API_KEY
+    });
+    
+    async execute() {
+        try {
+            const response = await this.aiInstance.chat.completions.create({
+                messages: [
+                    { 
+                        role: "system", 
+                        content: "You are a helpful assistant."
+                    }
+                ],
+                model: "deepseek-chat",
+              });
+            return response.choices[0].message.content;
+        } catch (error) {
+            console.error(error);
+            return error;
+        };   
+    };
+}
