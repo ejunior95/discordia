1551bfb649d159f240c092f612a6b3a806b45b7f
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-10 01:14:07 -0300

    feat: Criando estrutura da service com método para testar a integração com Chat GPT

--- src/chat-gpt/chat-gpt.service.ts
+++ src/chat-gpt/chat-gpt.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
-
+import OpenAI from "openai";
 @Injectable()
-export class ChatGptService {}
+export class ChatGptService {
+    aiInstance = new OpenAI();
+    
+    async execute() {
+        try {
+            const { output_text } = await this.aiInstance.responses.create({
+                model: "gpt-4o",
+                input: "Write a one-sentence bedtime story about a unicorn."
+            });
+            return output_text;
+        } catch (error) {
+            console.error(error);
+            return error;
+        };     
+    };
+}
