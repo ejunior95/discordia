2c278a68c48c0b02eb9e42931e4ba273a7a24502
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-10 01:14:52 -0300

    feat: Criando estrutura da service com método para testar a integração com Gemini

--- src/gemini/gemini.service.ts
+++ src/gemini/gemini.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { GoogleGenAI } from "@google/genai";
 
 @Injectable()
-export class GeminiService {}
+export class GeminiService {
+    aiInstance = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+    
+    async execute() {
+        try {
+            const { text } = await this.aiInstance.models.generateContent({
+                model: "gemini-2.0-flash",
+                contents: "Explain how AI works",
+            });
+            return text;
+        } catch (error) {
+            console.error(error);
+            return error;
+        };        
+    };
+
+}
