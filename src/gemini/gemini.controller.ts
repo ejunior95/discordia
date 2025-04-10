a4985212b960f5867ebc2a90f9f526292e992db2
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-10 01:14:41 -0300

    feat: Criando estrutura da controller com método para testar a integração com Gemini

--- src/gemini/gemini.controller.ts
+++ src/gemini/gemini.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Post, Req, Res } from '@nestjs/common';
+import { GeminiService } from './gemini.service';
+import { Request, Response } from 'express';
 
 @Controller('gemini')
-export class GeminiController {}
+export class GeminiController {
+    constructor(
+        private readonly geminiService: GeminiService,
+    ) {}
+
+    @Post('/test-message')
+    async testMessage(@Req() req: Request, @Res() res: Response) {
+        try {
+            const result = await this.geminiService.execute();
+            return res.status(HttpStatus.OK).json(result);
+        } catch (error) {
+            return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(error);
+        }
+    }
+}
