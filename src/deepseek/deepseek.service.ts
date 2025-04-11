f6d8ea9c6621506b7fcd082aa2d093a443bbfd3b
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-10 23:26:28 -0300

    feat: Refatorando a estrutura da service e atribuindo content personalizado para cada agente

--- src/deepseek/deepseek.service.ts
+++ src/deepseek/deepseek.service.ts
@@ -1,26 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import OpenAI from "openai";
+import { getCustomContent } from 'src/config/getCustomContent';
 @Injectable()
 export class DeepseekService {
-    aiInstance = new OpenAI({
+    private aiInstance = new OpenAI({
         baseURL: process.env.DEEPSEEK_API_BASE_URL,
         apiKey: process.env.DEEPSEEK_API_KEY
     });
+    customContent: string = getCustomContent('deepseek');
     
     async execute() {
         try {
             const response = await this.aiInstance.chat.completions.create({
+                model: "deepseek-v2",
                 messages: [
                     { 
                         role: "system", 
-                        content: "You are a helpful assistant."
+                        content: this.customContent
                     }
                 ],
-                model: "deepseek-chat",
+                temperature: 0.7
               });
             return response.choices[0].message.content;
         } catch (error) {
-            console.error(error);
+            console.error('Erro na chamada do Deepseek:', error);
             return error;
         };   
     };
