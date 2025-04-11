226136f516fe223d93cb9f0f0d1d005a141d022d
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-10 23:26:33 -0300

    feat: Refatorando a estrutura da service e atribuindo content personalizado para cada agente

--- src/gemini/gemini.service.ts
+++ src/gemini/gemini.service.ts
@@ -1,21 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { GoogleGenAI } from "@google/genai";
+import { getCustomContent } from 'src/config/getCustomContent';
 
 @Injectable()
 export class GeminiService {
-    aiInstance = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
-    
-    async execute() {
-        try {
-            const { text } = await this.aiInstance.models.generateContent({
-                model: "gemini-2.0-flash",
-                contents: "Explain how AI works",
-            });
-            return text;
-        } catch (error) {
-            console.error(error);
-            return error;
-        };        
-    };
+  private aiInstance = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+  customContent: string = getCustomContent('gemini');
 
+  async execute() {
+    try {
+      const { text } = await this.aiInstance.models.generateContent({
+        model: "gemini-flash-thinking",
+        contents: [
+            {
+                role: "system",
+                parts: [ { text: this.customContent } ]
+            }
+        ],
+      });
+      return text;
+    } catch (error) {
+      console.error('Erro na chamada do Gemini:', error);
+      return error;
+    };
+  };
 }
