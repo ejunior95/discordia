f5a2c5fbddc8f24c06b1d84d7928770e590da4ff
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-11 01:39:52 -0300

    feat: Adicionando pergunta no completions da aiInstance

--- src/deepseek/deepseek.service.ts
+++ src/deepseek/deepseek.service.ts
@@ -1,27 +1,39 @@
 import { Injectable } from '@nestjs/common';
-import OpenAI from "openai";
+import axios from 'axios';
 import { getCustomContent } from 'src/config/getCustomContent';
 @Injectable()
 export class DeepseekService {
-    private aiInstance = new OpenAI({
-        baseURL: process.env.DEEPSEEK_API_BASE_URL,
-        apiKey: process.env.DEEPSEEK_API_KEY
-    });
+    apiUrl = process.env.DEEPSEEK_API_BASE_URL;
+    apiKey = process.env.DEEPSEEK_API_KEY;
+    httpClient = axios.create({ baseURL: this.apiUrl });
     customContent: string = getCustomContent('deepseek');
     
-    async execute() {
+    async execute(question: string) {
         try {
-            const response = await this.aiInstance.chat.completions.create({
-                model: "deepseek-v2",
-                messages: [
-                    { 
-                        role: "system", 
-                        content: this.customContent
+            const response = await this.httpClient.post(
+                'chat/completions',
+                {
+                    model: 'deepseek-chat',
+                    messages: [
+                        { 
+                            role: 'system', 
+                            content: this.customContent 
+                        },
+                        { 
+                            role: 'user', 
+                            content: question 
+                        }
+                    ],
+                    temperature: 0.7
+                },
+                {
+                    headers: {
+                        'Authorization': `Bearer ${this.apiKey}`,
+                        'Content-Type': 'application/json'
                     }
-                ],
-                temperature: 0.7
-              });
-            return response.choices[0].message.content;
+                }
+            );
+            return response.data.choices[0].message.content;
         } catch (error) {
             console.error('Erro na chamada do Deepseek:', error);
             return error;
