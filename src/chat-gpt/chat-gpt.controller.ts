c9f0bb5b577cff0ff9dbe5c5e371789c5cb5ca76
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-11 01:38:24 -0300

    feat: Implementando validação de body e parametro de pergunta

--- src/chat-gpt/chat-gpt.controller.ts
+++ src/chat-gpt/chat-gpt.controller.ts
@@ -11,7 +11,13 @@ export class ChatGptController {
     @Post('/test-message')
     async testMessage(@Req() req: Request, @Res() res: Response) {
         try {
-            const result = await this.chatGptService.execute();
+            const { question } = req.body;
+            if (!question || !isNaN(question)) {
+                return res.status(HttpStatus.BAD_REQUEST).json({
+                    message: 'Pergunta não enviada ou inválida!'
+                });    
+            }
+            const result = await this.chatGptService.execute(question);
             return res.status(HttpStatus.OK).json(result);
         } catch (error) {
             return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(error);
