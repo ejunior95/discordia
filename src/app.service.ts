de6570ce4e47d605b1946ec232e4d26a6f53a157
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-11 01:37:43 -0300

    feat: Importando services

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,15 @@
+import { ChatGptService } from './chat-gpt/chat-gpt.service';
+import { DeepseekService } from './deepseek/deepseek.service';
 import { Injectable } from '@nestjs/common';
+import { GeminiService } from './gemini/gemini.service';
 
 @Injectable()
 export class AppService {
+  constructor(
+    private readonly chatGptService: ChatGptService,
+    private readonly deepseekService: DeepseekService,
+    private readonly geminiService: GeminiService
+  ) {}
   getHello(): string {
     return 'Hello World!';
   }
