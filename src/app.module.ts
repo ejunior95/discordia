6931876f6e17d31b430f2f23ddd295233ab4ec70
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-11 01:37:33 -0300

    feat: Importando services

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,13 @@ import { AppService } from './app.service';
 import { ChatGptModule } from './chat-gpt/chat-gpt.module';
 import { DeepseekModule } from './deepseek/deepseek.module';
 import { GeminiModule } from './gemini/gemini.module';
+import { DeepseekService } from './deepseek/deepseek.service';
+import { GeminiService } from './gemini/gemini.service';
+import { ChatGptService } from './chat-gpt/chat-gpt.service';
 
 @Module({
   imports: [ChatGptModule, DeepseekModule, GeminiModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DeepseekService, GeminiService, ChatGptService],
 })
 export class AppModule {}
