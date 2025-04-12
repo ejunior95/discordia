f2cd274869ef2dc70ddd2b7950b5e58eb8f34aad
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-12 00:50:40 -0300

    feat: Atualizando imports dos novos módulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,29 @@ import { GeminiModule } from './gemini/gemini.module';
 import { DeepseekService } from './deepseek/deepseek.service';
 import { GeminiService } from './gemini/gemini.service';
 import { ChatGptService } from './chat-gpt/chat-gpt.service';
+import { HttpModule } from '@nestjs/axios';
+import { Agent } from 'https';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ChatGptModule, DeepseekModule, GeminiModule],
+  imports: [
+    HttpModule.register({
+      httpsAgent: new Agent({
+        requestCert: true,
+        rejectUnauthorized: true
+      }),
+    }),
+    ConfigModule,
+    ChatGptModule, 
+    DeepseekModule, 
+    GeminiModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, DeepseekService, GeminiService, ChatGptService],
+  providers: [
+    AppService, 
+    DeepseekService, 
+    GeminiService, 
+    ChatGptService,
+  ],
 })
 export class AppModule {}
