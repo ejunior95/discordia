d86810ce3cb9ead73e1e0de8703f8f52fd48c4b0
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-15 19:30:32 -0300

    fix: Movendo diretórios para modules

--- src/gemini/gemini.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GeminiController } from './gemini.controller';
-import { GeminiService } from './gemini.service';
-
-@Module({
-  controllers: [GeminiController],
-  providers: [GeminiService]
-})
-export class GeminiModule {}
