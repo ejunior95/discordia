97d0c5b90892353c12d4b806370125870be30e4b
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-16 00:11:48 -0300

    feat: Criando service grok

---
+++ src/modules/grok/grok.service.ts
@@ -0,0 +1,36 @@
+import Anthropic from '@anthropic-ai/sdk';
+import { Injectable } from '@nestjs/common';
+import { getCustomContent } from 'src/config/getCustomContent';
+
+@Injectable()
+export class GrokService {
+  private aiInstance = new Anthropic({
+    apiKey: process.env.GROK_API_KEY,
+    baseURL: process.env.GROK_API_BASE_URL
+  });
+  customContent: string = getCustomContent('grok');
+
+  async execute(question: string) {
+    try {
+      const response = await this.aiInstance.messages.create({
+        model: 'grok-3-beta',
+        max_tokens: 2048,
+        messages: [
+          {
+            role: 'assistant',
+            content: this.customContent
+          },
+          {
+            role: 'user',
+            content: question
+          },
+        ],
+        temperature: 0.7
+      });
+      return response.content[0];
+    } catch (error) {
+      console.error('Erro na chamada do Grok:', error);
+      return error;
+    };
+  };
+}
