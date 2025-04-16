189368b0bf9d8488512eac0013da9e4ec7196b3e
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-16 00:06:52 -0300

    feat: Refatorando app.service e add novo módulo Grok

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,22 +1,32 @@
-import { ChatGptService } from './chat-gpt/chat-gpt.service';
-import { DeepseekService } from './deepseek/deepseek.service';
 import { Injectable } from '@nestjs/common';
-import { GeminiService } from './gemini/gemini.service';
+import { ChatGptService } from './modules/chat-gpt/chat-gpt.service';
+import { DeepseekService } from './modules/deepseek/deepseek.service';
+import { GeminiService } from './modules/gemini/gemini.service';
+import { GrokService } from './modules/grok/grok.service';
 
 @Injectable()
 export class AppService {
   constructor(
     private readonly chatGptService: ChatGptService,
     private readonly deepseekService: DeepseekService,
-    private readonly geminiService: GeminiService
+    private readonly geminiService: GeminiService,
+    private readonly grokService: GrokService,
   ) {}
 
   async askToAll(question: string) {
     try {
+      const grokResponse = await this.grokService.execute(question);
       return {
-        'chat-gpt': await this.chatGptService.execute(question),
-        'gemini': await this.geminiService.execute(question),
-        'deepseek': await this.deepseekService.execute(question),
+        'chat-gpt': {
+          response: await this.chatGptService.execute(question)
+        },
+        'gemini':  await this.geminiService.execute(question),
+        'deepseek': {
+          response: await this.deepseekService.execute(question)
+        },
+        'grok': {
+          response: grokResponse.text
+        },
       }
     } catch (error) {
       console.error('Erro no retorno em um ou mais dos agentes: ', error);
@@ -28,14 +38,24 @@ export class AppService {
     try {
       const response = {};
       if (agent === 'deepseek') {
-        response[agent] = await this.deepseekService.execute(question);
-      }
+        response[agent] = {
+          response: await this.deepseekService.execute(question)
+        };
+      };
       if (agent === 'gemini') {
-        response[agent] = await this.geminiService.execute(question);
-      }
+        response[agent] = await this.geminiService.execute(question)
+      };
       if (agent === 'chat-gpt') {
-        response[agent] = await this.chatGptService.execute(question);
-      }
+        response[agent] = {
+          response: await this.chatGptService.execute(question)
+        };
+      };
+      if (agent === 'grok') {
+        const grokResponse = await this.grokService.execute(question);
+        response[agent] = {
+          response: grokResponse.text
+        };
+      };
       return response;
     } catch (error) {
       console.error(error);
