729342edd40a7be1203c52806e8f24d291b8f705
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-17 01:51:25 -0300

    feat: Alterando estrutura do componente personalizado Navbar

--- front/src/custom-components/Navbar.tsx
+++ front/src/custom-components/Navbar.tsx
@@ -1,14 +1,21 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import Logo from "/discordia-logo.png"
+import { ModeToggle } from "@/components/mode-toggle"
 
 export const Navbar = () => {
   return (
-    <nav className="w-full absolute top-0 left-0 flex align-middle justify-between">
-        <img src={Logo} className="w-16 grayscale-100" alt="logo-discordia" />
-        <Avatar className="w-12 h-12">
+    <nav className="p-4 flex w-full justify-between bg-background text-foreground">
+        <div className="flex gap-3">
+          <img src={Logo} className="w-14 grayscale-100" alt="logo-discordia" />
+          <span className="text-2xl font-semibold tracking-tighter py-1">DiscordIA</span>
+        </div>
+        <div className="flex gap-6">
+          <ModeToggle />
+          <Avatar>
             <AvatarImage src="https://github.com/shadcn.png" />
-            <AvatarFallback>J</AvatarFallback>
-        </Avatar>
+            <AvatarFallback>JR</AvatarFallback>
+          </Avatar>
+        </div>
     </nav>
   )
 }
