43b036f75ae3767381a861a5900542a53ad4af12
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-17 10:20:17 -0300

    feat: Alterando estilos do componente ChatBody

--- front/src/custom-components/ChatBody.tsx
+++ front/src/custom-components/ChatBody.tsx
@@ -2,10 +2,10 @@ import { Button } from "@/components/ui/button"
 import { SendHorizontal } from "lucide-react"
 export const ChatBody = () => {
     return(
-        <section className="flex-col h-full items-center-safe justify-center w-220">
-                <p className="text-4xl font-semibold tracking-tighter text-center">Boa noite, Junior.</p>
-                <p className="text-4xl font-semibold tracking-tighter text-center"> Faça sua pergunta e veja as IAs disputarem pelo seu voto!</p>
-                <div className="w-220 h-36 bg-secondary rounded-2xl mt-8 p-2 relative">
+        <section className="w-220">
+                <p className="text-4xl font-semibold tracking-tighter text-center select-none">Boa noite, Junior.</p>
+                <p className="text-4xl font-semibold tracking-tighter text-center select-none"> Faça sua pergunta e veja as IAs disputarem pelo seu voto!</p>
+                <div className="w-220 h-36 bg-secondary rounded-xl mt-6 p-2 relative">
                     <textarea 
                         name="textareaQuestion" 
                         id="text-question" 
@@ -13,8 +13,8 @@ export const ChatBody = () => {
                         placeholder="O que você quer saber?" 
                         className="px-2 w-full outline-0 text-lg text-foreground resize-none" 
                     />
-                    <Button variant="default" size="icon" className="cursor-pointer absolute right-2 bottom-2">
-                        <SendHorizontal className="h-[1.2rem] w-[1.2rem]" />
+                    <Button variant="outline" size="icon" className="cursor-pointer absolute right-2 bottom-2">
+                        <SendHorizontal className="h-[1.5rem] w-[1.5rem]" />
                     </Button>
                 </div>
         </section>
