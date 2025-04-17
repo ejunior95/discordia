a5e07804540f73af76a32055ab26ceef871ed332
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-17 10:19:41 -0300

    feat: Centralizando objetos na tela

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -6,9 +6,9 @@ function App() {
   return (
     <ThemeProvider defaultTheme="system" storageKey="discordia-theme-select">
       <Navbar />
-      <div className='h-dvh w-full flex-col'>
+      <section className='w-full h-dvh flex-col place-content-center justify-items-center absolute top-18 left-0'>
         <ChatBody />
-      </div>
+      </section>
     </ThemeProvider>
   )
 }
