a3f314d8c70d0806dc8441108a0766e95f8f0b9a
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-17 13:52:45 -0300

    feat: Formatando estilização da sextion

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -6,7 +6,17 @@ function App() {
   return (
     <ThemeProvider defaultTheme="system" storageKey="discordia-theme-select">
       <Navbar />
-      <section className='w-full h-dvh flex-col place-content-center justify-items-center absolute top-18 left-0'>
+      <section 
+        className='
+          w-full 
+          h-dvh 
+          flex-col 
+          place-content-center 
+          justify-items-center 
+          absolute 
+          top-18 
+          left-0'
+        >
         <ChatBody />
       </section>
     </ThemeProvider>
