cbb77e6d4aedf65189a4de06030b3c3dea2219b2
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-18 01:17:01 -0300

    feat: Separando página de chat

---
+++ front/src/pages/Chat.tsx
@@ -0,0 +1,23 @@
+import { ChatBody } from "@/custom-components/ChatBody"
+import { Navbar } from "@/custom-components/Navbar"
+
+export default function Chat () {
+    return(
+        <>
+            <Navbar />
+            <section 
+            className='
+              w-full 
+              h-dvh 
+              flex-col 
+              place-content-center 
+              justify-items-center 
+              absolute 
+              top-18 
+              left-0'
+            >
+                <ChatBody />
+            </section>
+        </>
+    )
+}
