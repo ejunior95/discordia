0ac1b481bbe27841a23845ed4ce0ee6feb2f91e5
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-18 15:14:18 -0300

    feat: Definindo rotas

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,17 +1,19 @@
 import { ThemeProvider } from './components/theme-provider';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Suspense, lazy } from "react";
+import Loader from './custom-components/Loader';
 
+const Home = lazy(() => import('./pages/Home'));
 const Chat = lazy(() => import('./pages/Chat'));
 
 export default function App() {
   return (
     <ThemeProvider defaultTheme="system" storageKey="discordia-theme-select">
       <BrowserRouter>
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<Loader />}>
           <Routes>
-            <Route path="/" element={<Chat />} />
-            {/* <Route path="/about" element={<About />} /> */}
+            <Route path="/" element={<Home />} />
+            <Route path="/chat" element={<Chat />} />
             {/* Nested route with layout
             <Route path="/dashboard" element={<DashboardLayout />}>
               <Route path="stats" element={<Stats />} />
