6fbe188161fe7284cb428be812f7f8aa2e3d8244
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-18 15:16:41 -0300

    feat: Ajustando tabulação dos atributos da tag

--- front/src/pages/Chat.tsx
+++ front/src/pages/Chat.tsx
@@ -5,17 +5,7 @@ export default function Chat () {
     return(
         <>
             <Navbar />
-            <section 
-            className='
-              w-full 
-              h-dvh 
-              flex-col 
-              place-content-center 
-              justify-items-center 
-              absolute 
-              top-18 
-              left-0'
-            >
+            <section className='w-full h-dvh flex-col place-content-center justify-items-center absolute top-18 left-0'>
                 <ChatBody />
             </section>
         </>
