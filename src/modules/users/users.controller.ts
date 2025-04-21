954cbecf9fb060b991065bee58b9d8dcb02c9478
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-20 22:57:02 -0300

    feat: Importando DTOs

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -8,14 +8,15 @@ import {
     Body,
   } from '@nestjs/common';
   import { UsersService } from './users.service';
-  import { User } from './entities/user.entity';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
   
   @Controller('users')
   export class UsersController {
     constructor(private readonly usersService: UsersService) {}
   
     @Post()
-    create(@Body() body: Partial<User>) {
+    create(@Body() body: CreateUserDto) {
       return this.usersService.create(body);
     }
   
@@ -30,7 +31,7 @@ import {
     }
   
     @Put(':id')
-    update(@Param('id') id: string, @Body() body: Partial<User>) {
+    update(@Param('id') id: string, @Body() body: UpdateUserDto) {
       return this.usersService.update(id, body);
     }
   
