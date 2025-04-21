ffe27fd0961a4c20981b49e7566eaad21e6ea248
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-21 01:44:50 -0300

    feat: Alterando estrutura da controller e adc rota de atualização de img avatar de user

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,43 +1,67 @@
 import {
-    Controller,
-    Get,
-    Post,
-    Put,
-    Delete,
-    Param,
-    Body,
-  } from '@nestjs/common';
-  import { UsersService } from './users.service';
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Patch,
+  Param,
+  Body,
+  UploadedFile,
+  UseInterceptors,
+  BadRequestException,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
-  
-  @Controller('users')
-  export class UsersController {
-    constructor(private readonly usersService: UsersService) {}
-  
-    @Post()
-    create(@Body() body: CreateUserDto) {
-      return this.usersService.create(body);
-    }
-  
-    @Get()
-    findAll() {
-      return this.usersService.findAll();
-    }
-  
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-      return this.usersService.findOne(id);
-    }
-  
-    @Put(':id')
-    update(@Param('id') id: string, @Body() body: UpdateUserDto) {
-      return this.usersService.update(id, body);
-    }
-  
-    @Delete(':id')
-    remove(@Param('id') id: string) {
-      return this.usersService.remove(id);
-    }
+import { S3Service } from 'src/shared/s3.service';
+import { multerOptions } from 'src/shared/file.upload.config';
+import { ObjectId } from 'mongodb';
+
+@Controller('users')
+export class UsersController {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly s3Service: S3Service,
+  ) {}
+
+  @Post()
+  create(@Body() body: CreateUserDto) {
+    return this.usersService.create(body);
   }
-  
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(id, body);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(id);
+  }
+
+  @Patch(':id/avatar')
+  @UseInterceptors(FileInterceptor('avatar', multerOptions))
+  async uploadAvatar(
+    @Param('id') id: string,
+    @UploadedFile() file?: Express.Multer.File,
+  ) {
+    if (!file) {
+      throw new BadRequestException('Arquivo de avatar é obrigatório');
+    }
+
+    const url = await this.s3Service.uploadFile(file, `avatars/${id}`);
+    return this.usersService.update(id, { avatar: url });
+  }
+}
