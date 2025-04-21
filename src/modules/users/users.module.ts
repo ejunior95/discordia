6b934a837f97cd14d5f6bfda7e1d9f8e5d53594a
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-21 01:45:20 -0300

    feat: Adc import service S3

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
+import { S3Service } from 'src/shared/s3.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, S3Service],
 })
 export class UsersModule {}
