49021f834be72fa541add24de4714db0f7245281
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-21 04:54:13 -0300

    feat: Add service no modulo de usuarios

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -4,11 +4,12 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
 import { S3Service } from 'src/shared/s3.service';
+import { EmailService } from 'src/shared/email.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService, S3Service],
+  providers: [UsersService, S3Service, EmailService],
   exports: [UsersService],
 })
 export class UsersModule {}
