7cc8813cff9c4f1172cfd0d79fe5fd7a647db145
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-21 22:59:36 -0300

    feat: importando novas services no users.module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -5,11 +5,17 @@ import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
 import { S3Service } from 'src/shared/s3.service';
 import { EmailService } from 'src/shared/email.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService, S3Service, EmailService],
+  providers: [
+    UsersService, 
+    S3Service, 
+    EmailService, 
+    JwtService
+  ],
   exports: [UsersService],
 })
 export class UsersModule {}
