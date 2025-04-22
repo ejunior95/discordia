2a89e9c670e9b9e13e0c0ee7103a223438230f6b
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-21 22:58:14 -0300

    feat: Adc novos imports no constructor da controller auth e o método verify

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,10 +1,18 @@
-import { Controller, Post, Body, BadRequestException } from '@nestjs/common';
+import { Controller, Post, Body, BadRequestException, Get, Query } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dtos/login.dto';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '../users/users.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly jwtService: JwtService,
+    private readonly usersService: UsersService,
+    private configService: ConfigService
+  ) {}
 
   @Post('login')
   async login(@Body() loginDto: LoginDto) {
@@ -14,4 +22,22 @@ export class AuthController {
       throw new BadRequestException(`Erro ao realizar login - ${error}`);
     }
   }
+
+  @Get('verify')
+  async verifyEmail(@Query('token') token: string) {
+    try {
+      const secretEmail = this.configService.get<string>('EMAIL_VERIFICATION_SECRET');
+      if(!secretEmail) throw new BadRequestException('Secret de verificação de email não encontrado');
+
+      const payload = this.jwtService.verify(token, {
+        secret: secretEmail,
+      });
+
+      await this.usersService.verifyUserEmail(payload.email);
+      return { message: 'Email verificado com sucesso!' };
+    } catch (error) {
+      throw new BadRequestException('Token inválido ou expirado');
+    }
+  }
+
 }
