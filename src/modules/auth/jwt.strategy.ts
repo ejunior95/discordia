aefcec9480f37c7f98da0ce32dfa4e469b34fffd
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-23 00:11:00 -0300

    feat: Atualizando jwt strategy

--- src/modules/auth/jwt.strategy.ts
+++ src/modules/auth/jwt.strategy.ts
@@ -1,19 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
+import { Request } from 'express';
+import { UsersService } from '../users/users.service';
+import { UserResponseDto } from '../users/dtos/response-user.dto';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly config: ConfigService) {
+  constructor(
+    private readonly config: ConfigService,
+    private readonly usersService: UsersService
+  ) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (req: Request) => req?.cookies?.access_token,
+      ]),
       ignoreExpiration: false,
       secretOrKey: config.get('JWT_SECRET') as string,
     });
   }
 
-  async validate(payload: any) {
-    return { userId: payload.sub, email: payload.email };
+  async validate(payload: any): Promise<UserResponseDto> {
+    const user = await this.usersService.findOne(payload.sub);
+    if (!user) {
+      throw new UnauthorizedException('Usuário não encontrado');
+    }
+  
+    return {
+      id: user._id.toString(),
+      name: user.name,
+      email: user.email,
+      avatar: user.avatar,
+      createdAt: user.created_at,
+    };
   }
 }
