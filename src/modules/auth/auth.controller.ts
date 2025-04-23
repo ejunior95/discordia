91ead90640c41d3877f74700ac2e6cdb01e2f1e0
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-23 00:09:49 -0300

    feat: Atualizando metodos de login e logout

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Post, Body, BadRequestException, Get, Query } from '@nestjs/common';
+import { Controller, Post, Body, BadRequestException, Get, Query, UseGuards, Req, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dtos/login.dto';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../users/users.service';
 import { ConfigService } from '@nestjs/config';
+import { AuthGuard } from '@nestjs/passport';
+import { CurrentUser } from 'src/decorators/current-user.decorator';
+import { Request, Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -15,13 +18,34 @@ export class AuthController {
   ) {}
 
   @Post('login')
-  async login(@Body() loginDto: LoginDto) {
-    try {
-      return await this.authService.login(loginDto);
+  async login(@Body() loginDto: LoginDto, @Res({ passthrough: true }) res: Response) {
+  try {
+    const result = await this.authService.login(loginDto);
+
+    res.cookie('access_token', result.access_token, {
+      httpOnly: true,
+      sameSite: 'lax', // ou 'none' se for ambiente com https
+      secure: false, // true se for produção com HTTPS
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 dias
+    });
+
+    return result.user;
     } catch (error) {
       throw new BadRequestException(`Erro ao realizar login - ${error}`);
     }
   }
+
+  @Post('logout')
+  logout(@Res() res: Response) {
+    res.clearCookie('access_token', {
+      httpOnly: true,
+      sameSite: 'lax', // ou 'none' se estiver usando HTTPS e domínio diferente
+      secure: false, // true se for produção com HTTPS
+      // secure: process.env.NODE_ENV === 'production',
+    });
+    return res.send({ message: 'Logout realizado com sucesso' });
+  }
+
 
   @Get('verify')
   async verifyEmail(@Query('token') token: string) {
@@ -38,6 +62,13 @@ export class AuthController {
     } catch (error) {
       throw new BadRequestException('Token inválido ou expirado');
     }
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('me')
+  getMe(@Req() req: Request) {
+    const user = req.user as any;
+    return user;
   }
 
 }
