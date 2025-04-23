f7f758aff868f86c43c29ae08b560208aff75b02
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-23 00:08:22 -0300

    feat: Habilitando CORS com parametros e cookie-parser

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-   app.enableCors();
+
+  app.use(cookieParser());
 
    app.useGlobalPipes(new ValidationPipe());
 
-   // app.enableCors({
-   //   origin: 'http://localhost:5173',
-   //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-   //   allowedHeaders: 'Content-Type,Authorization',
-   // });
+   app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+  
 
   await app.listen(process.env.PORT ?? 3000);
 }
