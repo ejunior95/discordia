5cd72b02863707a026cfca777b4add48f4409a3b
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-23 22:08:05 -0300

    feat: Adicionando pipes, cors e cookie-parser

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,15 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   app.use(cookieParser());
-
-   app.useGlobalPipes(new ValidationPipe());
-
-   app.enableCors({
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors({
     origin: 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
   
-
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
