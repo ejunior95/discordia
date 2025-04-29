a9b8b4b7804ae28d3fe9ca650b0c49cb19cce50a
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-29 12:29:45 -0300

    feat: Renomeando ConversationMessage para ChatHistory nome da instancia do repositorio

--- src/modules/chat-gpt/chat-gpt.module.ts
+++ src/modules/chat-gpt/chat-gpt.module.ts
@@ -4,14 +4,14 @@ import { ChatGptService } from './chat-gpt.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { IA_Agent } from 'src/entities/agent.entity';
 import { Question } from 'src/entities/question.entity';
-import { ConversationMessage } from 'src/entities/chat-history.entity';
+import { ChatHistory } from 'src/entities/chat-history.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       IA_Agent, 
       Question,
-      ConversationMessage,
+      ChatHistory,
     ]),
   ],
   controllers: [ChatGptController],
