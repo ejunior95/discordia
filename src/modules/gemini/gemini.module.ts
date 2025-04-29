0fc5de1654abc248d91263f71af96c35db3dbd26
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-29 02:28:10 -0300

    feat: Importando entidades

--- src/modules/gemini/gemini.module.ts
+++ src/modules/gemini/gemini.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { GeminiController } from './gemini.controller';
 import { GeminiService } from './gemini.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { IA_Agent } from 'src/entities/agent.entity';
+import { Question } from 'src/entities/question.entity';
+import { ConversationMessage } from 'src/entities/chat-history.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      IA_Agent, 
+      Question,
+      ConversationMessage,
+    ]),
+  ],
   controllers: [GeminiController],
   providers: [GeminiService]
 })
