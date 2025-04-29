e65adf9f663c96d88316aa373c218453118857ae
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-29 02:28:22 -0300

    feat: Importando entidades

--- src/modules/grok/grok.module.ts
+++ src/modules/grok/grok.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { GrokController } from './grok.controller';
 import { GrokService } from './grok.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { IA_Agent } from 'src/entities/agent.entity';
+import { Question } from 'src/entities/question.entity';
+import { ConversationMessage } from 'src/entities/chat-history.entity';
 
 @Module({
+    imports: [
+        TypeOrmModule.forFeature([
+          IA_Agent, 
+          Question,
+          ConversationMessage,
+        ]),
+    ],
     controllers: [GrokController],
     providers: [GrokService]
 })
