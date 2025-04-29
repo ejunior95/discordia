062a61698d056c68fcb377add8564a344ac19686
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-29 02:00:03 -0300

    feat: Alterando estrutura da service grok

--- src/modules/grok/grok.service.ts
+++ src/modules/grok/grok.service.ts
@@ -1,38 +1,58 @@
 import Anthropic from '@anthropic-ai/sdk';
-import { Injectable } from '@nestjs/common';
+import { MessageParam } from '@anthropic-ai/sdk/resources';
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { getCustomContent } from 'src/utils/getCustomContent';
 
 @Injectable()
 export class GrokService {
-  private aiInstance = new Anthropic({
-    apiKey: process.env.GROK_API_KEY,
-    baseURL: process.env.GROK_API_BASE_URL
-  });
-  customContent: string = getCustomContent('grok');
+  private readonly logger = new Logger(GrokService.name);
+  private readonly aiInstance: Anthropic;
+  private readonly customContent: string;
+  private readonly baseURL: string;
+  private readonly apiKey: string;
 
-  async execute(question: string) {
+  constructor(
+    private readonly configService: ConfigService,
+  ) {
+    this.apiKey = this.configService.get<string>('GROK_API_KEY') ?? '';
+    this.baseURL = this.configService.get<string>('GROK_API_BASE_URL') ?? '';
+    if (!this.apiKey) {
+      this.logger.error('GROK_API_KEY não configurada');
+      throw new Error('Configuração da API Grok ausente.');
+    }
+    this.aiInstance =  new Anthropic({
+      apiKey: this.apiKey,
+      baseURL: this.baseURL
+    });
+    this.customContent = getCustomContent('grok');
+  }
+
+  async execute(question: string, history: { role: "user" | "assistant"; content: string; }[]): Promise<{ response: string }> {
     try {
+      const messages: MessageParam[] = [
+        ...history,
+        { role: 'user', content: question },
+      ];
+
       const response = await this.aiInstance.messages.create({
         model: 'grok-3-beta',
-        max_tokens: 250,
-        messages: [
-          {
-            role: 'assistant',
-            content: this.customContent
-          },
-          {
-            role: 'user',
-            content: question
-          },
-        ],
+        max_tokens: 100,
+        system: this.customContent,
+        messages,
         temperature: 0.7
       });
+
       // @ts-ignore
-      const { text } = response.content[0]
-      return { response: text };
+      const assistantReply = response.content[0].text;
+      return { response: assistantReply };
     } catch (error) {
-      console.error('Erro na chamada do Grok:', error);
-      return error;
-    };
-  };
+      console.error('Erro na chamada do Grok:', {
+        message: error.message,
+        status: error.status,
+        details: error
+      });
+      throw new Error('Erro na requisição para o Grok.');
+    }
+  }
 }
