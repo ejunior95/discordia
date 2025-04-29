d727d4236aa368f1008ef44317423d3dca4072eb
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-29 02:27:29 -0300

    feat: Importando entidades

--- src/modules/chat-gpt/chat-gpt.module.ts
+++ src/modules/chat-gpt/chat-gpt.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ChatGptController } from './chat-gpt.controller';
 import { ChatGptService } from './chat-gpt.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { IA_Agent } from 'src/entities/agent.entity';
+import { Question } from 'src/entities/question.entity';
+import { ConversationMessage } from 'src/entities/chat-history.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      IA_Agent, 
+      Question,
+      ConversationMessage,
+    ]),
+  ],
   controllers: [ChatGptController],
   providers: [ChatGptService]
 })
