6aaad9b9c11634db1530f04ba4f205a8495cc8e2
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-29 02:28:06 -0300

    feat: Replicando metodos do appService

--- src/modules/gemini/gemini.controller.ts
+++ src/modules/gemini/gemini.controller.ts
@@ -2,24 +2,34 @@ import { Controller, HttpStatus, Post, Req, Res, UseGuards } from '@nestjs/commo
 import { GeminiService } from './gemini.service';
 import { Request, Response } from 'express';
 import { AuthGuard } from '@nestjs/passport';
+import { UserResponseDto } from '../users/dtos/response-user.dto';
 
 @Controller('gemini')
 export class GeminiController {
-    constructor(
-        private readonly geminiService: GeminiService,
-    ) {}
+    constructor(private readonly geminiService: GeminiService) {}
 
     @UseGuards(AuthGuard('jwt'))
     @Post('/test-message')
-    async testMessage(@Req() req: Request, @Res() res: Response) {
+    async testMessage(@Req() req: Request & { user: UserResponseDto }, @Res() res: Response) {
         try {
             const { question } = req.body;
+            const userId = req.user.id
             if (!question || !isNaN(question)) {
                 return res.status(HttpStatus.BAD_REQUEST).json({
                     message: 'Pergunta não enviada ou inválida!'
                 });    
             }
-            const result = await this.geminiService.execute(question);
+
+            const history = await this.geminiService.getRecentHistory(userId, 10);
+            const result = await this.geminiService.execute(question, history);
+            await this.geminiService.saveMessage(userId, 'user', question);
+            await this.geminiService.saveMessage(
+                userId, 
+                'assistant', 
+                result.response ? result.response : '', 
+                'gemini'
+            );
+
             return res.status(HttpStatus.OK).json(result);
         } catch (error) {
             return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(error);
