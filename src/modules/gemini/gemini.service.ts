e57b94ab196bcd7167c35ac36f82b144233e2be5
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-29 02:05:16 -0300

    feat: Alterando estrutura da service gemini

--- src/modules/gemini/gemini.service.ts
+++ src/modules/gemini/gemini.service.ts
@@ -1,34 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { GoogleGenAI } from "@google/genai";
 import { getCustomContent } from 'src/utils/getCustomContent';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class GeminiService {
-  private aiInstance = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
-  customContent: string = getCustomContent('gemini');
+  private readonly logger = new Logger(GeminiService.name);
+  private aiInstance: GoogleGenAI;
+  private customContent = getCustomContent('gemini');
 
-  async execute(question: string) {
+  constructor(private readonly configService: ConfigService) {
+    const apiKey = this.configService.get<string>('GEMINI_API_KEY');
+    if (!apiKey) {
+      this.logger.error('GEMINI_API_KEY não configurada');
+      throw new Error('Configuração da API Gemini ausente.');
+    }
+    this.aiInstance = new GoogleGenAI({apiKey});
+  }
+
+  async execute(question: string, history: { role: 'user' | 'assistant'; content: string }[]): Promise<{ response: string }> {
     try {
-      const fullPrompt = `${this.customContent} Dito isso, minha pergunta é ${question}`
+      const contents = [
+        { role: 'user', parts: [{ text: this.customContent }] },
+        ...history.map((msg) => ({
+          role: msg.role === 'user' ? 'user' : 'assistant',
+          parts: [{ text: msg.content }],
+        })),
+        { role: 'user', parts: [{ text: question }] },
+      ];
+
       const { text } = await this.aiInstance.models.generateContent({
         model: "gemini-2.0-flash",
-        contents: [
-            {
-                role: "user",
-                parts: [ { text: fullPrompt } ]
-            },
-        ],
+        contents,
         config: {
-          maxOutputTokens: 250,
+          maxOutputTokens: 100,
           temperature: 0.7
         }
       });
-      return {
-        response: text
-      };
+
+      return { response: text ? text : '' };
     } catch (error) {
-      console.error('Erro na chamada do Gemini:', error);
-      return error;
-    };
-  };
+      this.logger.error('Erro na chamada do Gemini:', error);
+      throw error;
+    }
+  }
 }
