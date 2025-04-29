482e1e7023e1c995b4a0bd8703636374e9208d8e
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-29 01:55:36 -0300

    feat: Alterando estrutura da controller deepseek

--- src/modules/deepseek/deepseek.controller.ts
+++ src/modules/deepseek/deepseek.controller.ts
@@ -1,20 +1,39 @@
-import { Body, Controller, HttpException, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, HttpException, HttpStatus, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { DeepseekService } from './deepseek.service';
 import { AuthGuard } from '@nestjs/passport';
+import { AppService } from 'src/app.service';
+import { UserResponseDto } from '../users/dtos/response-user.dto';
+import { Request, Response } from 'express';
 
 @Controller('deepseek')
 export class DeepseekController {
-  constructor(private readonly deepSeekService: DeepseekService) {}
+  constructor(
+    private readonly deepSeekService: DeepseekService,
+    private readonly appService: AppService,
+  ) {}
 
   @UseGuards(AuthGuard('jwt'))
   @Post('/test-message')
-  async testMessage(@Body('question') question: string) {
-    if (!question || typeof question !== 'string' || question.trim().length === 0 || !/[a-zA-Z]/.test(question)) {
-      throw new HttpException('Pergunta não enviada ou inválida!', HttpStatus.BAD_REQUEST);
-    }
+  async testMessage(@Req() req: Request & { user: UserResponseDto }, @Res() res: Response) {
     try {
-      const result = await this.deepSeekService.execute(question);
-      return result;
+      const { question } = req.body;
+      const userId = req.user.id;
+    
+      if (!question || !isNaN(question)) {
+        return res.status(HttpStatus.BAD_REQUEST).json({
+            message: 'Pergunta não enviada ou inválida!'
+        });    
+      }
+      const history = await this.appService.getRecentHistory(userId, 10);
+      const result = await this.deepSeekService.execute(question, history);
+      await this.appService.saveMessage(userId, 'user', question);
+      await this.appService.saveMessage(
+          userId, 
+          'assistant', 
+          result.response ? result.response : '', 
+          'deepseek'
+      );
+      return res.status(HttpStatus.OK).json(result);
     } catch (error) {
       console.error('❌ Erro no controller ao consultar o Deepseek:', error);
       throw new HttpException('Erro ao consultar o Deepseek.', HttpStatus.INTERNAL_SERVER_ERROR);
