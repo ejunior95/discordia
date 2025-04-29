e61271b45998fbc5e1f73a4a44dc994e1fcfd821
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-04-29 01:55:00 -0300

    feat: Alterando estrutura da service chat-gpt

--- src/modules/chat-gpt/chat-gpt.service.ts
+++ src/modules/chat-gpt/chat-gpt.service.ts
@@ -1,35 +1,44 @@
-import { Injectable } from '@nestjs/common';
-import OpenAI from "openai";
+import OpenAI from 'openai';
+import { Injectable, Logger } from '@nestjs/common';
 import { getCustomContent } from 'src/utils/getCustomContent';
+import { ConfigService } from '@nestjs/config';
+import { ChatCompletionMessageParam } from 'openai/resources/chat';
+
 @Injectable()
 export class ChatGptService {
-    private aiInstance = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
-    customContent: string = getCustomContent('chat-gpt');
-    
-    async execute(question: string) {
-        try {
-            const response = await this.aiInstance.chat.completions.create({
-                model: 'gpt-4o',
-                messages: [
-                  {
-                    role: 'system',
-                    content: this.customContent
-                  },
-                  {
-                    role: 'user',
-                    content: question
-                  },
-                ],
-                max_tokens: 250,
-                temperature: 0.7
-              });
-            
-            return {
-              response: response.choices[0].message.content
-            };
-        } catch (error) {
-            console.error('Erro na chamada da OpenAI:', error);
-            return error;
-        };     
-    };
+  private readonly logger = new Logger(ChatGptService.name);
+  private aiInstance: OpenAI;
+  private customContent = getCustomContent('chat-gpt');
+
+  constructor(private readonly configService: ConfigService) {
+    const apiKey = this.configService.get<string>('OPENAI_API_KEY');
+    if (!apiKey) {
+      this.logger.error('OPENAI_API_KEY não configurada');
+      throw new Error('Configuração da API ChatGPT ausente.');
+    }
+    this.aiInstance = new OpenAI({ apiKey });
+  }
+
+  async execute(question: string, history: { role: 'user' | 'assistant'; content: string }[]) {
+    try {
+      const messages: ChatCompletionMessageParam[] = [
+        { role: 'system', content: this.customContent },
+        ...history,
+        { role: 'user', content: question },
+      ];
+
+      const response = await this.aiInstance.chat.completions.create({
+        model: 'gpt-4o',
+        messages,
+        max_tokens: 100,
+        temperature: 0.7,
+      });
+
+      const assistantReply = response.choices[0].message.content;
+      return { response: assistantReply };
+    } catch (error) {
+      console.error('Erro na chamada do ChatGPT:', error);
+      throw error;
+    }
+  }
 }
