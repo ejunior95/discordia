1e8b363912c7028f91658966c8dca0095df666a4
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-05-06 00:25:14 -0300

    feat: Incluindo log de execução app

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,8 @@ async function bootstrap() {
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
   
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`🚀 App running on port ${port}`);
 }
 bootstrap();
