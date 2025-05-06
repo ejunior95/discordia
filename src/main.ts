4cc3d8f8c039bf81fbef0f1107ce53db8dab2059
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-05-06 01:12:39 -0300

    feat: Correção no listen do app

--- src/main.ts
+++ src/main.ts
@@ -8,14 +8,14 @@ async function bootstrap() {
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://discordia.app.br'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
   
   const port = process.env.PORT || 3000;
-  await app.listen(port);
-  console.log(`🚀 App running on port ${port}`);
+  await app.listen(port, '0.0.0.0');
+  console.log(`🚀 App running on port ${port} and listening on 0.0.0.0`);
 }
 bootstrap();
