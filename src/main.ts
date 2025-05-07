bf75caaa82443e54fc9b08a2caadf3ab323fae8b
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-05-06 22:16:31 -0300

    feat: Alterando comentario

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
   
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
-  console.log(`🚀 App running on port ${port} and listening on 0.0.0.0`);
+  console.log(`🚀 App running on port ${port}`);
 }
 bootstrap();
