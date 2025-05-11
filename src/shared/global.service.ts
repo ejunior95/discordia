16135900de7ec71929267ca2473170cabcbdf0cb
C1327749 Edvaldo de Ramos Junior <c1327749@interno.bb.com.br>
2025-05-11 01:31:21 -0300

    feat: Criando metodos e constantes compartilhados

---
+++ src/shared/global.service.ts
@@ -0,0 +1,3 @@
+export const ALLOWED_AGENTS = ['deepseek', 'gemini', 'chat-gpt', 'grok'];
+
+export const ALLOWED_CONTEXTS = ['chat', 'chess', 'hangman-chooser', 'hangman-guesser', 'jokenpo', 'rpg', 'rap-battle']
